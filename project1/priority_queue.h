29a5f24ff77d6ed091a8949dcb8d87e1de39f61a
limdh09 <limdh0416@gmail.com>
2019-02-13 14:35:55 +0900

    Update priority_queue.h
    member name changed

--- project1/priority_queue.h
+++ project1/priority_queue.h
@@ -5,7 +5,7 @@
 template <typename T> 
 class TemplatePriorityQueue {
 	private:
-		std::priority_queue<T> storage;
+		std::priority_queue<T> storage_;
 	public:
 		templatePriorityQueue();
 		~templatePriorityQueue();
