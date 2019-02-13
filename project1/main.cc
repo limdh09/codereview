3c87aa38659584246c30f4518f43ec1f227bc0ec
limdh09 <limdh0416@gmail.com>
2019-02-13 14:32:04 +0900

    Update main.cc
    code changed following class name change

--- project1/main.cc
+++ project1/main.cc
@@ -5,23 +5,23 @@
 using namespace std;
 
 template <typename T>
-void pushQueue(templatePriorityQueue<T>* Que, int data){
+void pushQueue(TemplatePriorityQueue<T>* Que, int data){
 	cout << "[+] Integer "<< data <<" pushed in the Priority Queue." << endl;
 	Que->push(data);
 }
 
 template<typename T>
-void popQueue(templatePriorityQueue<T>* Que){
+void popQueue(TemplatePriorityQueue<T>* Que){
 	cout << "[+] Integer "<< Que->top() << " will be poped from the Priority Queue." << endl;
 	Que->pop();
 }
 
 int main(int argc, char* argv[]){
-	templatePriorityQueue<int> priQueue;
+	TemplatePriorityQueue<int> priQueue;
 	int customNumber = 0;
 
 	//empty check
-	//NULL üũ �ʿ�
+	//NULL Ã¼Å© ÇÊ¿ä
 	if(priQueue.empty()){
 		cout << "[!] Priority Queue is empty - initalized" << endl;
 	}
